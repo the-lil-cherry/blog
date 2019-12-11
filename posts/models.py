39df0542a1c32ff1ddea172a71578d816f5fdef3
Anna Beatriz Serikyaku <annabserikyaku@gmail.com>
2019-12-10 21:27:44 -0300

    Fazendo formulario

--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 class Postagem(models.Model):
     
@@ -14,4 +15,21 @@ class Postagem(models.Model):
     texto_blod = models.CharField(max_length = 150)
     tema = models.CharField(max_length = 3, choices = opcao_tema)
     ativo = models.BooleanField(default = True)
+    
+    def __str__(self):
+        return self.nome
 
+class Pedido(models.Model):
+    metodo_pagamento = [
+        ('AV', 'Pagamento à vista'),
+        ('P2', 'Parcelamento em 2x'),
+        ('P3', 'Parcelamento em 3x')
+    ]
+    nome = models.CharField(max_length = 140)
+    email = models.EmailField()
+    cartao = models.IntegerField()
+    pagamento = models.CharField(max_length = 2, choices = metodo_pagamento)
+    criado_em = models.DateTimeField(default = timezone.now)
+
+    def __str__(self):
+        return self.nome
--- posts/views.py
+++ posts/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render
+from .forms import PedidoForm
 
 # Create your views here.
 def home(request):
-    nome = 'Anna'
-    lista = ['Matheus', 'Murilo', 'Nathally', 'Giovana' ]
-    contexto = {
-        'nome':nome,
-        'publicacao':lista
-    }
+    
     return render(request, 'home.html', contexto)
 
 def post(request):
     return render(request,'post.html')
 
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    form = PedidoForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        context = {
+            'msg':"Pedido deu certo!"
+        }
+        return render(request, 'cadastro.html', context)
+    context = {
+        'formulario':form
+    }
+    return render(request, 'cadastro.html', context)
